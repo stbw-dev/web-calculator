fa26f00dd3802f231115be873379b7562ddad3d4
stbw-dev <sbwinn@uw.edu>
2026-04-27 20:40:08 -0700

    Added additional comments for context and clarity on code.

--- script.js
+++ script.js
@@ -4,23 +4,26 @@ let example = [];
 
 const buttons = document.querySelectorAll('button');
 
+// Add event listener for each button
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
         handleClick(event.target);
     });
 });
 
+
+// Function for handling button clicks from user
 function handleClick(element) {
     let p = document.querySelector(".display");
     let isCalculated = false;
     let isInvalid = false;
 
-    // Need to check if Array is empty, you can't have an arithmitic operator
-    // without a preceding number
+    // Need to check if Array is empty, can't have a unary arithmitic operator
+    // without a preceding number (for this calculator)
     if (test.length === 0) {
         // need to check if innerText is a valid integer
-        // if true = append to array
-        // else = ignore
+        // if true, append to array
+        // else, set flag
         if (!isNaN(element.innerText)) {
             test += element.innerText;
         } else {
@@ -46,10 +49,10 @@ function handleClick(element) {
     if (element.innerText === 'C') {
         test = "";
         p.innerText = "0";
-    } else if (isCalculated) {
+    } else if (isCalculated) { // calculated flag
          p.innerText = test;
          test = "";
-    } else if (isInvalid) {
+    } else if (isInvalid) { // invalid flag
         p.innerText = "0";
         isInvalid = false;
     } else {
@@ -57,29 +60,33 @@ function handleClick(element) {
     }
 }
 
+
+// Converts a infix string to an equivalent postfix expression
+// represented as an array of strings
 function parseInfixToPostfix(string) {
-    let arr_output = [];
-    let stack = [];
-    let fixed_len = string.length;
-    let digit_str = "";
+    let arr_output = []; // array for holding postfix exp
+    let stack = []; // our "stack" for handling tokens
+    let digit_str = ""; // temporarily store operands
 
-    for (let i = 0; i < fixed_len; i++) {
-        let char = string[i];
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
 
+        // if it's a number, append it to our string
+        // otherwise, we have an operator
         if (!isNaN(char)) {
             digit_str += char;
-        } else {
-            arr_output.push(digit_str);
-            digit_str = "";
+        } else { 
+            arr_output.push(digit_str); // add our number to the array
+            digit_str = "";             // clear the string
 
             if (stack.length === 0) {
-                stack.push(char);
+                stack.push(char);       // stack is empty, so push the first token
             } else {
-                // char is an operator
-                // evaluate char against each operator in the top of stack
+                // evaluate token against each operator in the top of stack
                 // if precedence of char is < top, pop until char is > or stack is empty
+                // for the simplified calculator +/- will clear the whole stack
                 while (stack.length > 0) {
-                    let top = stack[stack.length - 1];
+                    const top = stack[stack.length - 1];
                     if ((char === '+' || char === '-')) {
                         arr_output.push(stack.pop());
                     } else if ((char === '*' || char === '/') && 
@@ -90,15 +97,18 @@ function parseInfixToPostfix(string) {
                     }
                 }
 
+                // push the token onto the stack 
                 stack.push(char);
             }
         }
     }
 
+    // add any remaining intger
     if (digit_str.length !== 0) {
         arr_output.push(digit_str);
     }
 
+    // add any remaining tokens
     while (stack.length !== 0) {
         arr_output.push(stack.pop());
     }
@@ -106,14 +116,19 @@ function parseInfixToPostfix(string) {
     return arr_output;
 }
 
+// calculates the value given a string representation
+// strings must be in this exact format
+// i.e., "1+5", "5*4-2", "10/5*2-1+10", etc.
 function calculateEquation(string) {
-    let array = parseInfixToPostfix(string)
+    let array = parseInfixToPostfix(string) // convert string to postfix
     let stack = [];
 
+    // keep pushing the contents of the array until we have nothing left
+    // to evaluate
     while(array.length > 0) {
-        if (!isNaN(array[0])) {
+        if (!isNaN(array[0])) {             // if we have a number, push it to the stack
             stack.push(array.shift());
-        } else {
+        } else {                            // otherwise it's a token, so perform the relevant operation
             let value = 0;
             let token = array.shift();
             let right_opp = stack.pop();
